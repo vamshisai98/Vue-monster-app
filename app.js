2bdeaad2710e59d6ad1decc625db1c2ec382bdd6
vamshisai98 <saivamshianirudh136@gmail.com>
2021-08-01 23:39:01 +0530

    Health Bar function working

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const app = Vue.creatApp({
+const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
     };
   },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
 
   methods: {
-    attackMaster() {
+    attackMonster() {
+      console.log("working");
       const attackValue = Math.random(Math.random() * (12 - 5)) + 5;
       this.monsterHealth -= attackValue;
       this.attackPlayer();
